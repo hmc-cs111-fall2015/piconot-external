c4247c5d775413c3c405ab3a3fdec2212d8e59bf
ZoabKapoor <zoab95@hotmail.com>
2015-10-14 16:26:41 -0700

    finished adding errors on surroundings

--- src/main/scala/picoPrecious/semantics/ErrorChecker.scala
+++ src/main/scala/picoPrecious/semantics/ErrorChecker.scala
@@ -10,6 +10,7 @@ package object ErrorChecker {
     class TooManySurroundingsException(message: String) extends Exception
     class NoSurroundingsException(message: String) extends Exception
     class AllWildcardsException(message: String) extends Exception
+    class DuplicateSurroundingsException(message: String) extends Exception
   
     def error_check(rules : List[RuleBuilder]): Unit = {
       rules.foreach { x => checkOneRule(x) }
@@ -17,6 +18,8 @@ package object ErrorChecker {
     
     def checkOneRule(rule: RuleBuilder): Unit = {
       checkNumSurroundings(rule)
+      checkNotAllWildcards(rule)
+      checkNoDuplicateSurroundings(rule)
     }
     
     def checkNotAllWildcards(rule: RuleBuilder): Unit = {
@@ -33,6 +36,21 @@ package object ErrorChecker {
       }
       if (rule.surroundings.directions.size == 0) {
         throw new NoSurroundingsException("You didn't specify any of the surroundings!")
+      }
+    }
+    
+    def checkNoDuplicateSurroundings(rule: RuleBuilder): Unit = {
+      var northCount: Int = 0
+      var eastCount: Int = 0
+      var westCount: Int = 0
+      var southCount: Int = 0
+      rule.surroundings.directions.foreach { 
+        x => if (x.dir == North) northCount += 1 else if (x.dir == East) eastCount += 1 
+        else if (x.dir == West) westCount += 1 else if (x.dir == South) southCount += 1
+        else throw new IllegalArgumentException("Surrounding direction must be North, South, East, or West")
+      }
+      if (northCount > 1 || eastCount > 1 || westCount > 1 || southCount > 1) {
+        throw new DuplicateSurroundingsException("You specified a surrounding more than once!")
       }
     }
 }
