1abca8ea66ec6b395602935351667cf77f129742
JustisAllen <JustisAllen@me.com>
2015-10-14 21:09:14 -0700

    Fixed stage error and wrote build.md

--- build.md
+++ build.md
@@ -1,1 +1,5 @@
 # Building and running Piconot
+
+1. Place your rules in the `piconot-external` folder.
+2. Update `piconot.scala` to point to the desires maze and rules.
+3. Compile and run the files.
--- src/main/scala/piconot/piconot.scala
+++ src/main/scala/piconot/piconot.scala
@@ -1,23 +1,19 @@
 package piconot
 
-import scalafx.application.JFXApp.PrimaryStage
 import picolib.semantics._
 import picolib.maze.Maze
 import parser.PicoParser
 import java.io.File
 import scalafx.application.JFXApp
-import picolib.maze.Maze
-import picolib.semantics._
-import java.io.File
 
-object Piconot extends App {
+object Piconot extends JFXApp {
   val maze = Maze("resources" + File.separator + "empty.txt")
   PicoParser(io.Source.fromFile("empty_code.txt").mkString) match {
     case PicoParser.Success(rules, _) ⇒ {    
                   object EmptyBot extends Picobot(maze, rules)
                   with TextDisplay with GUIDisplay
                   EmptyBot.run()
-                  
+                  stage = EmptyBot.mainStage
      }
     case e: PicoParser.NoSuccess  ⇒ println(e)
   }
